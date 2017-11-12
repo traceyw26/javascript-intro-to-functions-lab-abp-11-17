67d3e4d55c9a51932f8f8e91ed649e82fcd4ef8a
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-12 03:42:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
   return "I can't hear you!"
 }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string)
+  return "YES INDEED!"
+}
